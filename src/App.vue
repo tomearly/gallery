aef1cfda1707bae525ae40700fb13b0d4edf10fa
Tom Early <tom.early@gmail.com>
2020-06-30 19:23:24 +0100

    Refactor app.vue to include gallery vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,53 @@
 <template>
-  <div id="app">
-    
+  <div 
+    id="app"
+    v-if="photos.results"
+    class="gallery">
+    <thumbnail
+      v-for="photo of photos.results"
+      :key="photo.id"
+      :alt-description="photo.alt_description"
+      :profile-url="photo.user.links.html"
+      :thumb-url="photo.urls.thumb"
+      :thumb-link-url="photo.links.html"
+      :first-name="photo.user.first_name">
+    </thumbnail>
   </div>
 </template>
 
 <script>
+import dataService from "@/api/dataService";
+import Thumbnail from "@/components/Thumbnail";
+
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Thumbnail
+  },
+  mounted() {
+    this.loadPhotos();
+  },
+  methods: {
+    async loadPhotos() {
+      this.photos = await dataService.getPhotos(
+        "query=paris&color=black_and_white&orientation=landscape"
+      );
+    }
+  },
+  data() {
+    return {
+      photos: {}
+    };
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.gallery {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 }
 </style>
---
+++ src/components/Thumbnail.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="thumbnail">
+    <a :href="thumbLinkUrl"><img :alt="altDescription" :src="thumbUrl" /></a>
+    <ThumbnailOverlay
+      class="thumbnail-overlay"
+      :first-name="firstName"
+      :profile-url="profileUrl"
+      :thumb-link-url="thumbLinkUrl">
+    </ThumbnailOverlay>
+  </div>
+</template>
+
+<script>
+import ThumbnailOverlay from "@/components/ThumbnailOverlay";
+export default {
+  components: {
+    ThumbnailOverlay
+  },
+  props: {
+    altDescription: {
+      type: String
+    },
+    profileUrl: {
+      type: String
+    },
+    thumbUrl: {
+      type: String
+    },
+    thumbLinkUrl: {
+      type: String
+    },
+    firstName: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+  .thumbnail-overlay {
+    position: relative;
+    top: -30px;
+    background-color: rgba(255, 255, 255, 0.75);
+    padding: 8px;
+  }
+
+  .thumbnail-overlay:hover {
+    background-color: rgba(255, 255, 255, 1);
+  }
+  
+</style>
