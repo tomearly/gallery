eda1037b86af3730a68a8f4a1161a5c4e17bb15b
Tom Early <tom.early@gmail.com>
2020-07-01 20:06:02 +0100

    Refactored App.vue to use new Gallery component. Added zoom effect on image hover

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,3 +1,31 @@
 <template>
-  <div></div>
+  <div id="app" v-if="photos.results" class="gallery">
+    <thumbnail
+      v-for="photo of photos.results"
+      :key="photo.id"
+      :alt-description="photo.alt_description"
+      :profile-url="photo.user.links.html"
+      :thumb-url="photo.urls.thumb"
+      :thumb-link-url="photo.links.html"
+      :first-name="photo.user.first_name"
+    >
+    </thumbnail>
+  </div>
 </template>
+
+<script>
+import Thumbnail from "@/components/Thumbnail";
+
+export default {
+  name: "Gallery",
+  components: {
+    Thumbnail
+  },
+  props: {
+    photos: {
+      type: Object,
+      default: () => {}
+    }
+  }
+};
+</script>
